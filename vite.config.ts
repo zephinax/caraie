25603aa58b610c85ee0e4962f21c532e0ba1e7d3
Zephinax <zephinax@gmail.com>
2025-02-03 20:33:00 -0800

    test 4

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: './images/*',
-          dest: 'dist/images',
-        },
-      ],
-    }),
-  ],
+  plugins: [react()],
+  base: './',
+  publicDir: 'public',
 });
