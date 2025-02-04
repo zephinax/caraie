69d90176103559866d5de03a8fda27227140180c
Zephinax <zephinax@gmail.com>
2025-02-04 13:31:40 -0800

    test 7

--- vite.config.ts
+++ vite.config.ts
@@ -3,25 +3,12 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.webp'],
   build: {
-    assetsDir: 'assets',
     rollupOptions: {
       output: {
-        assetFileNames: (assetInfo: { name?: string }) => {
-          if (!assetInfo.name) return 'assets/[name]-[hash][extname]';
-
-          const info = assetInfo.name.split('.');
-          const extType = info[info.length - 1];
-
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            return `public/images/[name]-[hash][extname]`;
-          }
-
-          return `piblic/[name]-[hash][extname]`;
-        },
+        assetFileNames: 'assets/[name].[hash][extname]',
       },
     },
   },
+  publicDir: 'public',
 });
