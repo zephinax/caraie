6bf9306f4856b47b6a541d08e3edf9fac6b18316
Zephinax <zephinax@gmail.com>
2025-11-24 10:53:36 +0330

    chore(release): publish gh pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/caraie/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/caraie/">
       <App />
     </Router>
   </React.StrictMode>
