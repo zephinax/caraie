ceb39651ee5cc460b693168a0cdd03fe822a0b1a
Zephinax <zephinax@gmail.com>
2025-11-24 10:43:00 +0330

    chore(release): publish gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public',
+  publicDir: "public",
+  base: "/caraie/",
 });
